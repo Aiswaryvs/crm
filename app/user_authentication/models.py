28423cc97c879f8145b1e86be1a5784aaa9aa394
Aiswarya <aiswaryaaishu0001@gmail.com>
2023-05-25 20:38:00 +0530

    final commit

--- app/user_authentication/models.py
+++ app/user_authentication/models.py
@@ -3,8 +3,42 @@ from ast import mod
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import AbstractUser,BaseUserManager
+# from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    use_in_migrations = True
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     username=None
@@ -20,8 +54,10 @@ class User(AbstractUser):
     is_updated = models.DateTimeField(auto_now=False, auto_now_add=True)
     is_active =  models.BooleanField(default=False)
 
+    objects = CustomUserManager()
+
     USERNAME_FIELD ='email'
-    REQUIRED_FIELDS = ['first_name']
+    REQUIRED_FIELDS = []
 
  
 
